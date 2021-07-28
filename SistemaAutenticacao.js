fcc6686535f877448bdb99bb72e42e50838267b6
Levi-Magny <levi_mgy@gmail.edu.br>
2021-07-27 23:24:34 -0300

    Polimorfismo

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,4 +1,9 @@
 export class SistemaAutenticacao{
+    /*
+    comportamento polimórfico:
+    Não importa qual o tipo de funcionário tente logar,
+    desde que seja herde a classe generica "Funcionario".
+    */
     static login(funcionario, senha){
         return funcionario.senha == senha;
     }
